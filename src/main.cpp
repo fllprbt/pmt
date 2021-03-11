c0769ef20e80ea43dbc11bbfc5284691824f45b4
fllprbt <pgiouvanelis@gmail.com>
2021-03-11 02:10:14 +0100

    feat: decouple buffer size of pitch detection from sound input
    the frame count which fires every update of the sound input (PAWrapper) should be as low as possible. The main reason for this is that there will be features that will be built on top of these updates which work better with continuous streams of data (i.e. graphic updates based on sound input).

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,14 @@
 #include <AudioIO.h>
 #include <AudioIOVariantFactory.h>
 #include <PAWrapper.h>
+#include <algorithm>
 #include <chrono>
 #include <iostream>
 #include <thread>
 
 enum AudioBackends { PortAudio };
+
+#define PITCH_WINDOW 4096
 
 int main(int argc, char **argv) {
   AudioIOVariantFactory audioFactory;
@@ -16,11 +19,17 @@ int main(int argc, char **argv) {
   std::unique_ptr<AudioIO> pA = audioFactory.create(AudioBackends::PortAudio);
 
   // Create our Pitch Detection object
-  const AubioWrapper aubio(pA->getFrames(), pA->getSampleRate());
+  const AubioWrapper aubio(PITCH_WINDOW, pA->getSampleRate());
 
   // Write callback for when PortAudio has data
+  std::vector<SAMPLE> buffer(PITCH_WINDOW);
   AudioIO::RecordCBHandle callback = [&](std::vector<SAMPLE> &&v) {
-    std::cout << aubio.detectPitch(v) << std::endl;
+    const size_t vSize = v.size();
+    for (uint_t i = 0; i < PITCH_WINDOW - vSize; i++)
+      buffer[i] = buffer[i + vSize];
+
+    std::move(v.begin(), v.end(), buffer.end() - vSize);
+    std::cout << aubio.detectPitch(buffer) << std::endl;
   };
 
   pA->startStream(callback);
